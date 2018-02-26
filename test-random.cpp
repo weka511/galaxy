ae546b9bf460dce01f9449259aeae251b38879b5
Weka <simon@greenweaves.nz>
2018-02-26 21:35:16 +0000

    Issue #25 - test case for Matsumoto & Nishimura's MersenneTwister

--- test-random.cpp
+++ test-random.cpp
@@ -14,14 +14,84 @@
  * You should have received a copy of the GNU General Public License
  * along with this software.  If not, see <http://www.gnu.org/licenses/>
  *
- * This file exercises treecode. 
+ * This file exercises the random number generators. It shows that the build-in
+ * generators ignore the seed, but that Matsumoto & Nishimura's MersenneTwister does use seed.
+ *
  */
  
 
 #include <random>
+#include <iostream>
 #include "catch.hpp"
+#include "mt.h"
 
+TEST_CASE( "Random number generator Tests1", "[random1]" ) {
+	std::mt19937_64 generator;
+	std::srand(1);
+	std::cout << "A time seed produced: " << generator() << std::endl;
+	std::cout << "A time seed produced: " << generator() << std::endl;
+	std::cout << "A time seed produced: " << generator() << std::endl;
+}
 
-TEST_CASE( "Random number generator Tests", "[random]" ) {
+TEST_CASE( "Random number generator Tests2", "[random2]" ) {
+	std::mt19937_64 generator;
+	std::srand(2);
+	std::cout << "A time seed produced: " << generator() << std::endl;
+	std::cout << "A time seed produced: " << generator() << std::endl;
+	std::cout << "A time seed produced: " << generator() << std::endl;
+	}
+	
+TEST_CASE( "Random number generator Tests3", "[random3]" ) {
+	std::mt19937_64 generator;
+	std::srand(time(NULL));
+	std::cout << "A time seed produced: " << generator() << std::endl;
+	std::cout << "A time seed produced: " << generator() << std::endl;
+	std::cout << "A time seed produced: " << generator() << std::endl;
+	}
+	
+TEST_CASE( "Random number generator Tests4", "[random4]" ) {
+	std::mt19937_64 generator;
+	std::srand(time(NULL));
+	std::cout << "A time seed produced: " << generator() << std::endl;
+	std::cout << "A time seed produced: " << generator() << std::endl;
+	std::cout << "A time seed produced: " << generator() << std::endl;
+	
+}
 
+TEST_CASE( "MersenneTwister Tests1", "[mersenne1]" ) {
+	MersenneTwister mt;
+	mt.init_genrand(1);
+    mt.print();
+	std::cout << "Seed 1 produced: " << mt.random() << std::endl;
+	std::cout << "Seed 1 produced:  " << mt.random() << std::endl;
+	std::cout << "Seed 1 produced:  " << mt.random() << std::endl;
+}
+
+TEST_CASE( "MersenneTwister Tests2", "[mersenne2]" ) {
+	MersenneTwister mt;
+	mt.init_genrand(1);
+    mt.print();
+	std::cout << "Seed 1 produced: " << mt.random() << std::endl;
+	std::cout << "Seed 1 produced:  " << mt.random() << std::endl;
+	std::cout << "Seed 1 produced:  " << mt.random() << std::endl;
+}
+
+TEST_CASE( "MersenneTwister Tests3", "[mersenne3]" ) {
+	MersenneTwister mt;
+	mt.init_genrand(2);
+    mt.print();
+
+	std::cout << "Seed 2 produced: " << mt.random() << std::endl;
+	std::cout << "Seed 2 produced:  " << mt.random() << std::endl;
+	std::cout << "Seed 2 produced:  " << mt.random() << std::endl;
+}
+
+TEST_CASE( "MersenneTwister Tests4", "[mersenne4]" ) {
+	MersenneTwister mt;
+	mt.init_genrand(time(NULL));
+    mt.print();
+
+	std::cout << "Time Seed produced: " << mt.random() << std::endl;
+	std::cout << "Time Seed  produced:  " << mt.random() << std::endl;
+	std::cout << "Time Seed  produced:  " << mt.random() << std::endl;
 }
