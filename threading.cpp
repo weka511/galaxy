96255c19d32b599e35062864aee2dea1af2839eb
Weka <simon@greenweaves.nz>
2018-03-13 08:23:10 +0100

    Issue #9 support multiple runs

--- threading.cpp
+++ threading.cpp
@@ -13,6 +13,9 @@
  *
  * You should have received a copy of the GNU General Public License
  * along with this software.  If not, see <http://www.gnu.org/licenses/>
+ *
+ * This program was written to investigate issue 9, https://github.com/weka511/galaxy/issues/9,
+ * by looking at the executing time of a task that can be split over multiple threads.
  */
 #include <iostream>
 #include <limits>
@@ -20,37 +23,48 @@
 #include <thread>
 #include <time.h> 
 
-int N=std::numeric_limits<int>::max();
 
-int foo() {
-	for (int i=1;i< N;i++){
+int foo(int from, int to) {
+	for (int i=from;i< to;i++){
 		double x=std::log(i);
 		double y=std::exp(x);
 	}
 }
 
 int main() {
-	for (int i=0;i<10;i++) {
-
-		auto start = std::chrono::system_clock::now();
-		std::time_t start_time = std::chrono::system_clock::to_time_t(start);
-		if (i==0)
-			foo();
-		else {
-			N=std::numeric_limits<int>::max()/i;
-			std::thread* ts[i];
-			for (int j=0;j<i;j++)
-				ts[j]=new std::thread(foo);
-			for (int j=0;j<i;j++){
-				ts[j]->join();
-				delete ts[j];
+	for (int k=0;k<10;k++)
+		for (int i=0;i<15;i++) {
+			int from    = 0;
+			int to      = 0;
+			const int N = std::numeric_limits<int>::max();
+			
+			auto start             = std::chrono::system_clock::now();
+			std::time_t start_time = std::chrono::system_clock::to_time_t(start);
+			
+			if (i==0) {
+				to=N;
+				foo(1,to);
+			} else {
+				const int incr = N/i;
+				from           = 0;
+				to             = 0;
+				std::thread* ts[i];			
+				for (int j=0;j<i;j++){
+					from = to;
+					to   += incr;
+					if (j==i-1) to = N;
+					ts[j]=new std::thread(foo,from,to);
+				}
+				for (int j=0;j<i;j++){
+					ts[j]->join();
+					delete ts[j];
+				}
 			}
+			auto end = std::chrono::system_clock::now();
+			std::chrono::duration<double> elapsed_seconds = end-start;
+				 
+			std::cout<<i<<","<<  elapsed_seconds.count()<< std::endl;
 		}
-		auto end = std::chrono::system_clock::now();
-		std::chrono::duration<double> elapsed_seconds = end-start;
-			 
-		std::cout<<i<<","<< i*N<<"," << elapsed_seconds.count()<< std::endl;
-	}
  
     std::cout << "done" << std::endl;
     return 0;
