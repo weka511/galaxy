758c8f6fbc18a3efdea9260d649f7857e0e77e40
Weka <simon@greenweaves.nz>
2018-03-17 20:22:32 +0100

    Issue #9 Test with more bodies

--- timing.sh
+++ timing.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/bash
 for i in 1 2 3 4 5 6 7 8 9 10
  do
-    ./galaxy.exe --threads 4
-	./galaxy.exe --threads 2
-	./galaxy.exe
+    ./galaxy.exe --threads 4 --max_iter 10 --numbodies 100000  --img_iter 1
+	./galaxy.exe --threads 2 --max_iter 10 --numbodies 100000 --img_iter 1
+	./galaxy.exe --max_iter 10 --numbodies 100000 --img_iter 1
 
  done
  
