1c3c47aa17167deb417332a9f56ec8d591bba041
Weka <simon@greenweaves.nz>
2018-03-20 00:24:22 +0100

    Close #31: simple threshold to control colours

--- make_3d.py
+++ make_3d.py
@@ -32,6 +32,9 @@ def get_limits(xs,nsigma=3):
     mu=scipy.mean(xs)
     sigma=scipy.std(xs)
     return (mu-nsigma*sigma,mu+nsigma*sigma)
+    
+def colour_from_index(i,threshold=5000):
+    return 'b' if i<threshold else 'r'
 
 '''
 Plot points from input file
@@ -42,37 +45,35 @@ Plot points from input file
 I have had to workaround the problem of the legend function not supporting the type returned by a 3D scatter.
 See https://stackoverflow.com/questions/20505105/add-a-legend-in-a-3d-scatterplot-with-scatter-in-matplotlib for details.
 '''
-def plot(fname_in='kepler.csv',n=len(colours),m=sys.maxsize,scale_to_cube=False,scale_first_time_only=False,out='./imgs',nsigma=3,N=1000):    
+def plot(fname_in='kepler.csv',n=len(colours),m=sys.maxsize,scale_to_cube=False,out='./imgs',nsigma=3,N=1000,get_colour=colour_from_index):    
     pos = np.loadtxt(fname_in,delimiter=',')
     plt.figure(figsize=(20,10))
     ax = plt.gcf().add_subplot(111, aspect='equal', projection='3d')
     scatterproxies=[]
     labels=[]
-    limits_x_0=None
-    limits_y_0=None
-    limits_z_0=None
-    for i in range(n):
-        full_range=list(range(i,min(len(pos),m),n))
-        sample=full_range if N==None or N*n>=len(pos) else random.sample(full_range,N)
-        xs=[pos[j,0] for j in sample]
-        ys=[pos[j,1] for j in sample]
-        zs=[pos[j,2] for j in sample]
-        colour=colours[i%len(colours)]
-        ax.scatter(xs,ys,zs,edgecolor=colour,s=1)
-        if scale_to_cube:
-            ax.set_xlim(get_limits(xs,nsigma=nsigma))
-            ax.set_ylim(get_limits(ys,nsigma=nsigma))
-            ax.set_zbound(get_limits(zs,nsigma=nsigma))
-        if scale_first_time_only:
-            if i==0:
-                limits_x_0=get_limits(xs,nsigma=nsigma)
-                limits_y_0=get_limits(ys,nsigma=nsigma)
-                limits_z_0=get_limits(zs,nsigma=nsigma)
-            ax.set_xlim(limits_x_0)
-            ax.set_ylim(limits_y_0)
-            ax.set_zbound(limits_z_0)            
-        scatterproxies.append( lines.Line2D([0],[0], linestyle="none", c=colour, marker = 'o'))
-        labels.append('{0}'.format(i))
+    
+    xs=[pos[j,0] for j in range(len(pos))]
+    ys=[pos[j,1] for j in range(len(pos))]
+    zs=[pos[j,2] for j in range(len(pos))]
+    colour=[get_colour(j) for j in range(len(pos))]
+    ax.scatter(xs,ys,zs,edgecolor=colour,s=1)
+    if scale_to_cube:
+        ax.set_xlim(get_limits(xs,nsigma=nsigma))
+        ax.set_ylim(get_limits(ys,nsigma=nsigma))
+        ax.set_zbound(get_limits(zs,nsigma=nsigma))    
+    #for i in len(pos):
+        #full_range=list(range(i,min(len(pos),m),n))
+        #sample=full_range if N==None or N*n>=len(pos) else random.sample(full_range,N)
+
+        #colour=colours[i%len(colours)]
+        #ax.scatter(xs,ys,zs,edgecolor=colour,s=1)
+        #if scale_to_cube:
+            #ax.set_xlim(get_limits(xs,nsigma=nsigma))
+            #ax.set_ylim(get_limits(ys,nsigma=nsigma))
+            #ax.set_zbound(get_limits(zs,nsigma=nsigma))
+     
+        #scatterproxies.append( lines.Line2D([0],[0], linestyle="none", c=colour, marker = 'o'))
+        #labels.append('{0}'.format(i))
     plt.title(fname_in.replace('.csv',''));
     plt.xlabel('x')
     plt.ylabel('y')
@@ -95,8 +96,8 @@ def usage():
     print ('\t-o, --out\tPath name for images')
     print ('\t-g, --nsigma\tNumber of standard deviations in cube')
     print ('\t-N, --sample\tUsed to sample from data')
+    print ('\t-t, --colour_threshold\tUsed to assign colours to fifferent clusters')
     print ('\t-v, --movie\tMake movie (specify name of output file)')
-    print ('\t-y, --scale_first_time_only')
     
 if __name__=='__main__':
     try:
@@ -113,11 +114,11 @@ if __name__=='__main__':
         movie_maker             = 'ffmpeg.exe'
         framerate               = 1
         pattern                 = 'bodies%05d.png'
-        scale_first_time_only   = False
+        get_colour              = colour_from_index
         
         opts, args = getopt.getopt(sys.argv[1:],
-                                   'hm:n:scog:N:v:f:y',
-                                   ['help', 'points=','bodies=','show','cube','out=','nsigma=','sample=','movie=','img_freq=','scale_first_time_only'])
+                                   'hm:n:scog:N:v:f:t:',
+                                   ['help', 'points=','bodies=','show','cube','out=','nsigma=','sample=','movie=','img_freq=','colour_threshold'])
         for o,a in opts:
             if o in ['-h','--help']:
                 usage()
@@ -140,14 +141,14 @@ if __name__=='__main__':
                 movie=a
             elif o in ['-f','--img_freq']:
                 img_freq=int(a)
-            elif o in ['-y','--scale_first_time_only']:
-                scale_first_time_only=True
+            elif o in ['-t','--colour_threshold']:
+                get_colour=lambda i:colour_from_index(i,threshold=int(a))                
             else:
                 print ('Unexpected option {0} {1}'.format(o,a))
                 sys.exit(2) 
         for fname_in in args:
             if os.path.isfile(fname_in):
-                img_file=plot(fname_in=fname_in,n=n,m=m,scale_to_cube=scale_to_cube,scale_first_time_only=scale_first_time_only,out=out,N=N,nsigma=nsigma)
+                img_file=plot(fname_in=fname_in,n=n,m=m,scale_to_cube=scale_to_cube,out=out,N=N,nsigma=nsigma,get_colour=get_colour)
                 if i%img_freq==0:
                     print ('Created {0}'.format(img_file))
                 i+=1
@@ -156,7 +157,7 @@ if __name__=='__main__':
             elif os.path.isdir(fname_in):
                 for filename in os.listdir(fname_in):
                     if filename.endswith(".csv"):
-                        img_file=plot(fname_in=os.path.join(fname_in,filename),n=n,m=m,scale_to_cube=scale_to_cube,scale_first_time_only=scale_first_time_only,out=out,N=N,nsigma=nsigma)
+                        img_file=plot(fname_in=os.path.join(fname_in,filename),n=n,m=m,scale_to_cube=scale_to_cube,out=out,N=N,nsigma=nsigma,get_colour=get_colour)
                         if i%img_freq==0:
                             print ('Created {0}'.format(img_file))
                         i+=1
