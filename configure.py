b096f21b2cb25b8c3e6fc34970d2a7d318ce00bd
Simon Crase <simon@greenweaves.nz>
2019-10-07 09:38:12 +1300

    Issue #42 - coded up Plummer and linked to encode

--- configure.py
+++ configure.py
@@ -13,7 +13,7 @@
 # You should have received a copy of the GNU General Public License
 # along with this software.  If not, see <http://www.gnu.org/licenses/>
 
-import os, random, sys
+import os, random, sys, struct, math
 from shutil import copyfile
 
 def backup(output='config_new.text'):
@@ -22,7 +22,7 @@ def backup(output='config_new.text'):
         copyfile(output,backup_file)
         
 def configure(config_version = 1.0,
-              output         = 'config_new.text',
+              output         = 'config_new.txt',
               type           = 'plummer',
               soften         = 1.0,
               imag_freq      = 1,
@@ -30,11 +30,21 @@ def configure(config_version = 1.0,
               number_bodies  = 1000,
               path           = '.',
               radius         = 1.0,
-              theta          = 1.0):
+              theta          = 1.0,
+              iter           = 0,
+              dt             = 0.1,
+              G              = 1.0):
     backup(output=output)
     config_factory = create_config_factory(type)
     with open(output,'w') as f:
-        pass
+        f.write('Version={0}\n'.format(config_version))
+        f.write('iteration={0}\n'.format(iter))
+        f.write('theta={0}\n'.format(encode(theta)))
+        f.write('G={0}\n'.format(encode(G)))
+        f.write('dt={0}\n'.format(encode(dt)))
+        for body in config_factory(number_bodies=number_bodies):
+            f.write(','.join([encode(b) for b in body])+'\n' )
+            
 
 def create_config_factory(type):
     config_factory_dict = {'plummer':plummer}
@@ -44,26 +54,55 @@ def create_config_factory(type):
         print ('Unrecognized config type: {0}'.format(type))
         sys.exit(1)
         
-def plummer():
-    pass
+def plummer(number_bodies=100):
+    def create_pos(radius=1):
+        theta  = math.acos(random.uniform(-1,1))
+        phi    = random.uniform(0,2*math.pi)
+        x      = radius * math.sin(theta) * math.cos(phi)
+        y      = radius * math.sin(theta) * math.sin(phi)
+        z      = radius * math.cos(theta)
+        return (x,y,z)
+    
+    def create_velocity(radius=1):
+        x = 0.0
+        y = 0.1
+        while y > x*x*(1-x*x)**3.5:
+            x = random.uniform(0,1)
+            y = random.uniform(0,0.1)
+        velocity = x * math.sqrt(2.0) *(1 + radius*radius)**(-0.25)
+        theta    = math.acos(random.uniform(-1,1))
+        phi      = random.uniform(0,2*math.pi)
+        vx       = velocity * math.sin(theta) * math.cos(phi)
+        vy       = velocity * math.sin(theta) * math.sin(phi)
+        vz       = velocity * math.cos(theta)
+        return (vx,vy,vz)
+    
+    def create_body():
+        mass     = 1.0/number_bodies
+        radius   = 1.0/math.sqrt(random.uniform(0,1)**(-2/3) - 1)
+        x,y,z    = create_pos(radius=radius)
+        vx,vy,vz = create_velocity(radius=radius)
+        return [mass,x,y,z,vx,vy,vz]
+    
+    return [create_body() for i in range(number_bodies)]
 
-def serialize(x):
+def encode(x):
     return hex(struct.unpack('!q', struct.pack('!d',x))[0])[2:]
     
 if __name__=='__main__':
     import argparse
     parser = argparse.ArgumentParser('Configure galaxy.exe')
     parser.add_argument(      '--dt', type=float, default=0.1,       help='Step size for integration')
-    parser.add_argument(      '--output', default='config_new.text', help='Configuration file')
+    parser.add_argument(      '--output', default='config_new.txt', help='Configuration file')
     parser.add_argument(      '--type', default='plummer',           help='Used to initialize distribution')
     parser.add_argument('-a', '--soften', type=float, default=1.0,   help='Softening length')
     parser.add_argument('-i', '--image_freq',                        help='Controls frequency for logging')
     parser.add_argument('-m', '--max_iter',                          help='Number of iterations')
-    parser.add_argument('-n', '--number_bodies',                     help='Number of bodies')
+    parser.add_argument('-n', '--number_bodies', default=100,        help='Number of bodies')
     parser.add_argument('-p', '--path',                              help='Path for configuration files')
-    parser.add_argument('-r', '--radius',                            help='Initial Radius')
+    parser.add_argument('-r', '--radius', type=float, default=1.0,   help='Initial Radius')
     parser.add_argument('-s', '--seed',                              help='Initialize the random number generator')
-    parser.add_argument('-t', '--theta',                             help='Theta-criterion of the Barnes-Hut algorithm')
+    parser.add_argument('-t', '--theta', type=float, default=0.5,    help='Theta-criterion of the Barnes-Hut algorithm')
     args = parser.parse_args()
     random.seed(args.seed)
     configure(output        = args.output,
@@ -72,5 +111,6 @@ if __name__=='__main__':
               number_bodies = args.number_bodies,
               path          = args.path,
               radius        = args.radius,
-              theta         = args.theta)
+              theta         = args.theta,
+              dt            = args.dt)
     
