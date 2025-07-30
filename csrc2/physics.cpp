f377febab3022b14316c5a0a4860a6286da63f55
Weka <simon@greenweaves.nz>
2025-07-30 16:48:45 +1200

    #58 Encapsulate Physics

--- csrc2/physics.cpp
+++ csrc2/physics.cpp
@@ -22,80 +22,31 @@
 
 using namespace std;
 
-void AccelerationCalculator::visit(Particle & particle){
-	particle.reset_acceleration();
-}
 
-void AccelerationCalculator::visit_pair(Particle & particle1,Particle & particle2){
-	double x1,y1,z1;
-	particle1.getPos(x1,y1,z1);
-	double x2,y2,z2;
-	particle2.getPos(x2,y2,z2);
-	const double distance= sqrt(sqr(x1-x2)+sqr(y1-y2)+sqr(z1-z2));
-	double d_factor=1/(distance*distance*distance);
-	assert(particle1.get_mass()>0);
-	assert(particle2.get_mass()>0);
-	const double acc_x1 =  particle2.get_mass()*G*(x2-x1)*d_factor;
-	const double acc_y1 =  particle2.get_mass()*G*(y2-y1)*d_factor;
-	const double acc_z1 =  particle2.get_mass()*G*(z2-z1)*d_factor;
-	particle1.accumulate_acceleration(acc_x1,acc_y1,acc_z1);
-	const double acc_x2 =  particle1.get_mass()*G*(x1-x2)*d_factor;
-	const double acc_y2 =  particle1.get_mass()*G*(y1-y2)*d_factor;
-	const double acc_z2 =  particle1.get_mass()*G*(z1-z2)*d_factor;
-	particle2.accumulate_acceleration(acc_x2,acc_y2,acc_z2);
-	// check Newton's third law
-	assert( fabs(particle1.get_mass()*acc_x1+particle2.get_mass()*acc_x2)<=epsilon*(fabs(particle1.get_mass()*acc_x1)+fabs(particle2.get_mass()*acc_x2)));
-	assert( fabs(particle1.get_mass()*acc_y1+particle2.get_mass()*acc_y2)<=epsilon*(fabs(particle1.get_mass()*acc_y1)+fabs(particle2.get_mass()*acc_y2)));
-	assert( fabs(particle1.get_mass()*acc_z1+particle2.get_mass()*acc_z2)<=epsilon*(fabs(particle1.get_mass()*acc_z1)+fabs(particle2.get_mass()*acc_z2)));
-}
+const double Physics::seconds_in_one_year = 365.25*24*60*60;
 
-// ================================= legacy code starts here ====================================
+const double Physics::mass_earth = 5.9722e24;  // https://en.wikipedia.org/wiki/Earth_mass
+
+const double Physics::mass_sun = 1.98855e30;  // https://en.wikipedia.org/wiki/Solar_mass
+
+const double Physics::metres_per_AU = 149597870700; // https://en.wikipedia.org/wiki/Astronomical_unit
+
+const double Physics::au_per_m = 1/metres_per_AU;
+
+const double Physics::G_SI = 6.67408e-11; // https://en.wikipedia.org/wiki/Gravitational_constant
+
+const double Physics::G_solar_system = G_SI * au_per_m*au_per_m*au_per_m *(mass_sun)*(seconds_in_one_year *seconds_in_one_year);
 	
-void get_acceleration(unique_ptr<Particle[]> &particles, int n, double G) {
-	for (int i=0;i<n;i++)
-		particles[i].reset_acceleration();
-	for (int i=1;i<n;i++)
-		for (int j=0;j<i;j++)
-			get_acceleration_between_pair(particles[i],particles[j],G);
-}
-
-const double epsilon=1e-8;  // tolerance for checking Newton's 3rd law
-
-void get_acceleration_between_pair(Particle& p1,Particle& p2,double G) {
-	double x1,y1,z1;
-	p1.getPos(x1,y1,z1);
-	double x2,y2,z2;
-	p2.getPos(x2,y2,z2);
-	const double distance= sqrt(sqr(x1-x2)+sqr(y1-y2)+sqr(z1-z2));
-	double d_factor=1/(distance*distance*distance);
-	assert(p1.get_mass()>0);
-	assert(p2.get_mass()>0);
-	const double acc_x1 =  p2.get_mass()*G*(x2-x1)*d_factor;
-	const double acc_y1 =  p2.get_mass()*G*(y2-y1)*d_factor;
-	const double acc_z1 =  p2.get_mass()*G*(z2-z1)*d_factor;
-	p1.accumulate_acceleration(acc_x1,acc_y1,acc_z1);
-	const double acc_x2 =  p1.get_mass()*G*(x1-x2)*d_factor;
-	const double acc_y2 =  p1.get_mass()*G*(y1-y2)*d_factor;
-	const double acc_z2 =  p1.get_mass()*G*(z1-z2)*d_factor;
-	p2.accumulate_acceleration(acc_x2,acc_y2,acc_z2);
-	// check Newton's third law
-	assert( fabs(p1.get_mass()*acc_x1+p2.get_mass()*acc_x2)<=epsilon*(fabs(p1.get_mass()*acc_x1)+fabs(p2.get_mass()*acc_x2)));
-	assert( fabs(p1.get_mass()*acc_y1+p2.get_mass()*acc_y2)<=epsilon*(fabs(p1.get_mass()*acc_y1)+fabs(p2.get_mass()*acc_y2)));
-	assert( fabs(p1.get_mass()*acc_z1+p2.get_mass()*acc_z2)<=epsilon*(fabs(p1.get_mass()*acc_z1)+fabs(p2.get_mass()*acc_z2)));
-}
-
-
 /**
  *  Calculate kinetic energy for a system of particles
  */
-double get_kinetic_energy(unique_ptr<Particle[]> &particles, int n) {
-	double sum=0;
+double Physics::get_kinetic_energy(unique_ptr<Particle[]> &particles, int n) {
+	auto  sum=0.0;
 	for (int i=0;i<n;i++) {
-		auto vx=0.0;
-		auto vy= 0.0;
-		auto vz=0.0;
-		particles[i].getVel(vy,vz,vz);
-		sum+=particles[i].get_mass()*(sqr(vx)+sqr(vy)+sqr(vz));
+		auto mass = particles[i].get_mass();
+		auto velocity = particles[i].get_velocity();
+		for (int j=0;j<3;j++)
+			sum+=mass*sqr(velocity[j]);
 	}
 	return 0.5*sum;
 }
@@ -103,7 +54,7 @@ double get_kinetic_energy(unique_ptr<Particle[]> &particles, int n) {
 /**
  *  Calculate gravitational potential energy for a system of particles
  */
-double get_potential_energy(unique_ptr<Particle[]> &particles, int n,const double G,const double a) {
+double Physics::get_potential_energy(unique_ptr<Particle[]> &particles, int n,const double G,const double a) {
 	double sum=0;
 	for (int i=1;i<n;i++)
 		for (int j=0;j<i;j++)
@@ -116,32 +67,30 @@ double get_potential_energy(unique_ptr<Particle[]> &particles, int n,const doubl
 /**
  * Find linear momentum of a set of particles
  */
-void get_momentum(unique_ptr<Particle[]> &particles, int n,double& px,double& py,double &pz) { 
+void Physics::get_momentum(unique_ptr<Particle[]> &particles, int n,double& px,double& py,double &pz) { 
 	px=0;py=0;pz=0;
 	for (int i=0;i<n;i++){
-		double vx,vy,vz;
-		particles[i].getVel(vx,vy,vz);
 		const double m=particles[i].get_mass();
-		px+=m*vx;
-		py+=m*vy;
-		pz+=m*vz;
+		auto V = particles[i].get_velocity();
+		px += m*V[0];
+		py += m*V[1];
+		pz += m*V[2];
 	}
 }
 
 /**
  * Find centre of mass of a set of particles.
  */
-void get_centre_of_mass(unique_ptr<Particle[]> &particles, int n,double& x0,double& y0,double &z0) {
+void Physics::get_centre_of_mass(unique_ptr<Particle[]> &particles, int n,double& x0,double& y0,double &z0) {
 	x0=0; y0=0; z0=0;
 	double m0=0;
 	for (int i=0;i<n;i++){
-		double x,y,z;
-		particles[i].getPos(x,y,z);
+		auto X = particles[i].get_position();
 		const double m=particles[i].get_mass();
-		m0+=m;
-		x0+=m*x;
-		y0+=m*y;
-		z0+=m*z;
+		m0 += m;
+		x0 += m*X[0];
+		y0 += m*X[1];
+		z0 += m*X[2];
 	}
 	x0/=m0; y0/=m0; z0/=m0;
 }
@@ -149,33 +98,25 @@ void get_centre_of_mass(unique_ptr<Particle[]> &particles, int n,double& x0,doub
 /**
  * Find angular momentum of a set of particles
  */
-void get_angular_momentum(unique_ptr<Particle[]> &particles, int n,double& lx,double& ly,double &lz) {
+void Physics::get_angular_momentum(unique_ptr<Particle[]> &particles, int n,double& lx,double& ly,double &lz) {
 	lx=0;ly=0;lz=0;
 	double x0,y0,z0;
 	get_centre_of_mass(particles,n,x0,y0,z0);
 	for (int i=0;i<n;i++){
-		double x,y,z;
-		particles[i].getPos(x,y,z);
+		// double x,y,z;
+		auto X = particles[i].get_position();
 		const double m=particles[i].get_mass();
 		// Calculate unit radius vector from centre of mass
-		const double d=sqrt(dsq(x0,y0,z0,x,y,z));
-		const double rx=(x-x0)/d;
-		const double ry=(y-y0)/d;
-		const double rz=(z-z0)/d;
+		const double d=sqrt(Physics::dsq(x0,y0,z0,X[0],X[1],X[2]));
+		const double rx=(X[0]-x0)/d;
+		const double ry=(X[1]-y0)/d;
+		const double rz=(X[2]-z0)/d;
+				
+		auto V = particles[i].get_velocity();
 		
-		double vx,vy,vz;
-		particles[i].getVel(vx,vy,vz);
-		
-		lx+=m*(ry*vz-rz*vy);
-		ly+=m*(rz*vx-rx*vz);
-		lz+=m*(rx*vy-ry*vx);
+		lx+=m*(ry*V[2]-rz*V[1]);
+		ly+=m*(rz*V[0]-rx*V[2]);
+		lz+=m*(rx*V[1]-ry*V[0]);
 	}
 }
 
-void get_acceleration(double m,double x,double y,double z,double _x,double _y,double _z,double dsq,double a, double G,
-					double& acc_x, double& acc_y, double& acc_z){
-	const double d_factor=pow(dsq+a*a,-3/2);
-	acc_x=G*m*(x-_x)*d_factor;
-	acc_y=G*m*(y-_y)*d_factor;
-	acc_z=G*m*(z-_z)*d_factor;
-}
--- csrc2/physics.hpp
+++ csrc2/physics.hpp
@@ -25,63 +25,55 @@
 
 using namespace std;
 
-const double seconds_in_one_year = 365.25*24*60*60;
-
-const double mass_earth = 5.9722e24;  // https://en.wikipedia.org/wiki/Earth_mass
-
-const double mass_sun = 1.98855e30;  // https://en.wikipedia.org/wiki/Solar_mass
-
-const double metres_per_AU = 149597870700; // https://en.wikipedia.org/wiki/Astronomical_unit
-
-const double au_per_m = 1/metres_per_AU;
-
-const double G_SI = 6.67408e-11; // https://en.wikipedia.org/wiki/Gravitational_constant
-
-const double G_solar_system = G_SI * au_per_m*au_per_m*au_per_m *(mass_sun)*(seconds_in_one_year *seconds_in_one_year);
-
-class AccelerationCalculator: public Configuration::CompoundVisitor {
-	const double epsilon=1e-8;  // tolerance for checking Newton's 3rd law
-	double G = 1.0;
+class Physics {
   public:
-	virtual void visit(Particle & particle);
-	virtual void visit_pair(Particle & particle1,Particle & particle2);
+	static const double seconds_in_one_year;
+
+	static const double mass_earth;
+
+	static const double mass_sun ;  // https://en.wikipedia.org/wiki/Solar_mass
+
+	static const double metres_per_AU ; // https://en.wikipedia.org/wiki/Astronomical_unit
+
+	static const double au_per_m;
+
+	static const double G_SI ; // https://en.wikipedia.org/wiki/Gravitational_constant
+
+	static const double G_solar_system ;
+	
+	inline static double dsq(double x0,double y0,double z0,double x1,double y1,double z1) {return sqr(x0-x1) + sqr(y0-y1) + sqr(z0-z1);}
+	
+	/**
+	 *  Calculate kinetic energy for a system of particles
+	 */
+	double get_kinetic_energy(unique_ptr<Particle[]> &particles, int n);
+
+	/**
+	 *  Calculate gravitational potential energy for a system of particles
+	 */
+	double get_potential_energy(unique_ptr<Particle[]> &particles, int n,const double G,const double a);
+
+
+	/**
+	 * Find linear momentum of a set of particles
+	 */
+	void get_momentum(unique_ptr<Particle[]> &particles, int n,double& px,double& py,double &pz);
+
+	/**
+	 * Find angular momentum of a set of particles
+	 */
+	void get_angular_momentum(unique_ptr<Particle[]> &particles, int n,double& lx,double& ly,double &lz);
+
+	/**
+	 * Find centre of mass of a set of particles.
+	 */
+	void get_centre_of_mass(unique_ptr<Particle[]> &particles, int n,double& x0,double& y0,double &z0);
+
 };
 
 
-// ================================= legacy code starts here ====================================
-	
-void get_acceleration(unique_ptr<Particle[]> &particles, int n, double G);
-
-void get_acceleration_between_pair(Particle& p_i,Particle& p_j,double G);
-
-void get_acceleration(double m,double x,double y,double z,double _x,double _y,double _z,double dsq,double a, double G,
-					double& acc_x, double& acc_y, double& acc_z);
-/**
- *  Calculate kinetic energy for a system of particles
- */
-double get_kinetic_energy(unique_ptr<Particle[]> &particles, int n);
-
-/**
- *  Calculate gravitational potential energy for a system of particles
- */
-double get_potential_energy(unique_ptr<Particle[]> &particles, int n,const double G,const double a);
 
 
-/**
- * Find linear momentum of a set of particles
- */
-void get_momentum(unique_ptr<Particle[]> &particles, int n,double& px,double& py,double &pz);
 
-/**
- * Find angular momentum of a set of particles
- */
-void get_angular_momentum(unique_ptr<Particle[]> &particles, int n,double& lx,double& ly,double &lz);
-
-/**
- * Find centre of mass of a set of particles.
- */
-void get_centre_of_mass(unique_ptr<Particle[]> &particles, int n,double& x0,double& y0,double &z0);
-
-inline double dsq(double x0,double y0,double z0,double x1,double y1,double z1) {return sqr(x0-x1) + sqr(y0-y1) + sqr(z0-z1);}
 
 #endif  //_PHYSICS_HPP
