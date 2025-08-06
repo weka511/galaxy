0f35130cbb0c04783772adb6cecf0f6bbe95c731
Weka <simon@greenweaves.nz>
2025-08-07 11:36:38 +1200

    #60 Display orbits

--- scripts/plot_orbits.py
+++ scripts/plot_orbits.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-# Copyright (C) 2018-2025 Greenweaves Software Limited
+# Copyright 2025 Simon Crase
 #
 # This is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -14,147 +14,142 @@
 
 # You should have received a copy of the GNU General Public License
 # along with this software.  If not, see <http://www.gnu.org/licenses/>
-#
 
-# Companion to galaxy.exe - plot orbits of randomly selected stars
+'''Plot orbits from Barnes Hut Galaxy Simulator, galaxy.exe'''
 
-import argparse
-import os
-import os.path
-import re
-import sys
+from argparse import ArgumentParser
+from os import listdir
+from os.path import join
+from re import search
+from sys import exit
 import numpy as np
-import matplotlib.pyplot as plt
-import mpl_toolkits.mplot3d,random
+from matplotlib.pyplot import figure, show
+import mpl_toolkits.mplot3d
 
-# get_limits
-#
-# Used to establish limits for plotting.
-#
-# Parameters:
-#    xs    Data from one axis
-#    n     Controls the limits on data width
-#
-# Returns:  mean _/- n standard deviations
+def parse_args():
+    parser = ArgumentParser(description=__doc__)
+    parser.add_argument('--bodies','-b', type=int, default=1000, help='Number of bodies from simulation')
+    parser.add_argument('--dpi', type=int, default=300, help='Dots per inch for displaying and saving figure')
+    parser.add_argument('--norbits','-n', type=int, default=7, help='Number of orbits')
+    parser.add_argument('--maxsamples','-m', type=int, default=1000, help='Maximum number of sample per orbit (-1 to process all samples)')
+    parser.add_argument('--prefix', default='galaxy', help='Prefix for configuration files')
+    parser.add_argument('--suffix','-s', default='csv', help='Suffix for configuration files')
+    parser.add_argument('--delimiter','-d', default=',', help='Delimiter for fields in config file')
+    parser.add_argument('--nsigma','-g', type=int,  default=3, help='Number of standard deviations to use for scaling')
+    parser.add_argument('--seed', type=int,  default=None, help='Seed for random number generator')
+    parser.add_argument('--images','-i',  default='./figs', help='Path to store images')
+    parser.add_argument('--path','-p', default='../csrc/configs', help='Path for config files')
+    parser.add_argument('--show', action = 'store_true', help   = 'Show plot')
+    return parser.parse_args()
+
 
 def get_limits(xs,n=1):
+    '''
+    Used to establish limits for plotting.
+
+    Parameters:
+       xs    Data from one axis
+       n     Controls the limits on data width
+
+    Returns:  mean _/- n standard deviations
+    '''
     mean  = np.mean(xs)
     sigma = np.std(xs)
     return (mean-n*sigma,mean+n*sigma)
 
-# plot
-#
-# Plot orbits
-#
-#    Parameters:
-#        data      Positions of sampled points in all orbits
-#        selector  Used to selectd which points will be plotted
-#        colours   Colours used to distinguish orbits
-
-def plot(rr,
-         selector=[],
-         colours=['r','g','b','m','c','y','k'],
+def plot(Orbits,selector,
+         colours=['xkcd:purple','xkcd:green','xkcd:blue','xkcd:pink','xkcd:brown','xkcd:red',
+                  'xkcd:light blue', 'xkcd:teal', 'xkcd:orange', 'xkcd:light green', 'xkcd:magenta', 'xkcd:yellow'],
          n=2,
          images='.',
          linestyles = ['-', '--', '-.', ':'],
          dpi=300):
-    (m,data)=rr
-    if len(data)==0:
+    '''
+    Plot orbits
+    Parameters:
+        data      Positions of sampled points in all orbits
+        selector  Used to selectd which points will be plotted
+        colours   Colours used to distinguish orbits
+    '''
+    m,K,_ = Orbits.shape
+    if m == 0:
         print ('No data - check path')
-        return False
-    def get_coordinates(body,i):
-        return [d[body][i] for d in data]
-    plt.figure(figsize=(20,20))
-    ax = plt.gcf().add_subplot(111,  projection='3d')
-    xs=[]
-    ys=[]
-    zs=[]
-    for body in range(len(data[0])):
-        ax.plot(get_coordinates(body,0),get_coordinates(body,1),get_coordinates(body,2),
-                c=colours[body%len(colours)],
-                linestyle=linestyles[(body//len(colours))%len(linestyles)])
-        ax.scatter(get_coordinates(body,0)[0],get_coordinates(body,1)[0],get_coordinates(body,2)[0],c=colours[body%len(colours)],
-                       label="Body: {0}".format(selector[body]),s=50,marker='x')
-        xs.append(get_coordinates(body,0))
-        ys.append(get_coordinates(body,1))
-        zs.append(get_coordinates(body,2))
+        sys.exit(2)
 
-    ax.set_xlim(get_limits(xs,n=n))
-    ax.set_ylim(get_limits(ys,n=n))
-    ax.set_zlim(get_limits(zs,n=n))
-
-    plt.legend(loc='best')
-    plt.title('Orbits of {0} randomly selected stars out of {1}'.format(len(data[0]),m))
-    plt.savefig(os.path.join(images,'orbits-{1}-{0}.png'.format(len(data[0]),m)), dpi=dpi)
+    fig = figure(figsize=(20,20))
+    ax = fig.add_subplot(111,  projection='3d')
+    for k in range(K):
+        ax.plot(Orbits[:,k,0],Orbits[:,k,1],Orbits[:,k,2],
+                c=colours[k%len(colours)],
+                linestyle=linestyles[(k//len(colours)) % len(linestyles)])
+        ax.scatter(Orbits[0,k,0],Orbits[0,k,1],Orbits[0,k,2],
+                   c=colours[k%len(colours)],
+                   label=f'{k}',
+                   s=50,marker='x')
+    ax.legend(loc='best',title='Starts of Orbits')
+    ax.set_title(f'Orbits of {K} randomly selected stars out of {m}')
+    ax.set_xlabel('X')
+    ax.set_ylabel('Y')
+    ax.set_zlabel('Z')
+    ax.set_xlim(get_limits(Orbits[:,:,0],n=n))
+    ax.set_ylim(get_limits(Orbits[:,:,1],n=n))
+    ax.set_zlim(get_limits(Orbits[:,:,2],n=n))
+    fig.savefig(join(images,f'orbits-{m}-{K}.png'), dpi=dpi)
     return True
 
-# extract
-#
-# Extract data from configuration files
-#
-#    Parameters:
-#        config_path     Location of data
-#        selector        Used to select points that will be extracted
-#        maxsamples      Maximum number of points sampled in each orbit
-#        prefix          Prefix for configuration files
-#        suffix          Suffix for configuration files
-#        delimiter       Delimiter for fields in config file
-
 def extract(config_path = './configs/',
-            selector=[0,1,2,55,100,400],
+            k = 6,
             maxsamples=1000,
             prefix='bodies',
             suffix='csv',
-            delimiter=','):
-    m      = None
-    result = []
-    n      = len(os.listdir(config_path))   # Total number of points
-    skip   = 1                              # Used to skip over data so number of points won't exceed maxsamples
-    while maxsamples>0 and n//skip>maxsamples:
-        skip*=10
+            delimiter=',',
+            rng = np.random.default_rng()):
+    '''
+    Extract data from configuration files
+
+       Parameters:
+           config_path     Location of data
+           k
+           maxsamples      Maximum number of points sampled in each orbit
+           prefix          Prefix for configuration files
+           suffix          Suffix for configuration files
+           delimiter       Delimiter for fields in config file
+    '''
+    m = None
+    Result = None
+    n = len(listdir(config_path))   # Total number of points
+    skip = 1                              # Used to skip over data so number of points won't exceed maxsamples
+    while maxsamples > 0 and n//skip > maxsamples:
+        skip *= 10
 
     i = 0
-    for file_name in os.listdir(config_path):
-        match = re.search(r'{0}[0-9]+\.{1}'.format(prefix,suffix),file_name)
+    for file_name in listdir(config_path):
+        match = search(r'{0}[0-9]+\.{1}'.format(prefix,suffix),file_name)
         if match:
+            record = np.loadtxt(join(config_path,match.group(0)),delimiter=delimiter)
             if i%skip == 0:
-                positions = np.loadtxt(os.path.join(config_path,match.group(0)),delimiter=delimiter)
-                m         = len(positions)
-                result.append([positions[i] for i in selector])
-            i+=1
+                if m == None:
+                    m = len(record)
+                    selector = rng.choice(m,size=k)
+                    Result = np.zeros((len(listdir(config_path)),k,3))
+            Result[i,:,:] = record[selector,1:4]
+            i += 1
 
-    return (m,result)
+    return (Result,selector)
 
 
 if __name__=='__main__':
-    parser = argparse.ArgumentParser(description='Plot orbits from Barnes Hut Galaxy Simulator, galaxy.exe')
-    parser.add_argument('--bodies','-b', type=int, default=1000, help='Number of bodies from simulation')
-    parser.add_argument('--dpi', type=int, default=300, help='Dots per inch for displaying and saving figure')
-    parser.add_argument('--norbits','-n', type=int, default=7, help='Number of orbits')
-    parser.add_argument('--maxsamples','-m', type=int, default=1000,        help='Maximum number of sample per orbit (-1 to process all samples)')
-    parser.add_argument('--prefix',                    default='bodies',    help='Prefix for configuration files')
-    parser.add_argument('--suffix','-s',               default='csv',       help='Suffix for configuration files')
-    parser.add_argument('--delimiter','-d',            default=',',         help='Delimiter for fields in config file')
-    parser.add_argument('--nsigma','-g',    type=int,  default=3,           help='Number of standard deviations to use for scaling')
-    parser.add_argument('--seed',           type=int,  default=None,        help='Seed for random number generator')
-    parser.add_argument('--images','-i',               default='./imgs',    help='Path to store images')
-    parser.add_argument('--path','-p',                 default='./configs', help='Path for config files')
-    args = parser.parse_args()
+    args = parse_args()
 
-    random.seed(args.seed)
+    rng = np.random.default_rng(args.seed)
 
-    selector=random.sample(range(args.bodies),args.norbits)
-    if plot(
-        extract(
-            config_path = args.path,
-            selector    = selector,
-            maxsamples  = args.maxsamples,
-            prefix      = args.prefix,
-            suffix      = args.suffix,
-            delimiter   = args.delimiter),
-        selector = selector,
-        n        = args.nsigma,
-        images   = args.images,
-        dpi      = args.dpi):
+    Orbits,selector = extract(config_path = args.path,
+                              maxsamples = args.maxsamples,
+                              prefix = args.prefix,
+                              suffix = args.suffix,
+                              delimiter = args.delimiter,
+                              k = args.norbits)
+    plot(Orbits,selector)
 
-        plt.show()
+    if args.show:
+        show()
