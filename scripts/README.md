a822972ed24e13438d580627a82967fadd85b614
Weka <simon@greenweaves.nz>
2025-08-09 12:21:52 +1200

    #72 Wrote script to verify conservation of momentum (it isn't conserved)

--- scripts/README.md
+++ scripts/README.md
@@ -1,14 +1,15 @@
 # galaxy -- Python scripts
 
 
- File |  Purpose 
+ File |  Purpose
 ---------------------|---------------------------------------------------------------------------------------
-adze.py|Cut logfiles up and analyze data 
+adze.py|Cut logfiles up and analyze data
 collision.xml|Sample input for configure.py
-configure.py| Create configuration for galaxy.exe 
+configure.py| Create configuration for galaxy.exe
 galaxy.wpr| WingIE project file
-make_3d.py|Plot output from galaxy 
-make_img.py|Plot output from galaxy 
-plot_energy.py|Used to investigate distribution of energies - do we thermalize? 
-plot_orbits.py|select a few stars at random and plot their orbits 
-utils.py |Code shared by other python scripts 
+make_3d.py|Plot output from galaxy
+make_img.py|Plot output from galaxy
+momentum.py|Used to check whether momentum is being conserved -- Issue #72
+plot_energy.py|Used to investigate distribution of energies - do we thermalize?
+plot_orbits.py|select a few stars at random and plot their orbits
+utils.py |Code shared by other python scripts
---
+++ scripts/momenta.py
@@ -0,0 +1,94 @@
+#!/usr/bin/env python
+
+#   Copyright (C) 2025 Simon Crase
+
+#   This program is free software: you can redistribute it and/or modify
+#   it under the terms of the GNU General Public License as published by
+#   the Free Software Foundation, either version 3 of the License, or
+#   (at your option) any later version.
+
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+
+#  You should have received a copy of the GNU General Public License
+#  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+''' Template for Python programs'''
+
+from argparse import ArgumentParser
+from os.path import basename, join, splitext
+from time import time
+from re import compile
+import numpy as np
+from matplotlib import rc
+from matplotlib.pyplot import figure, show
+
+
+def parse_arguments():
+    '''Parse command line arguments'''
+    parser = ArgumentParser(__doc__)
+    parser.add_argument('--seed',type=int,default=None,help='Seed for random number generator')
+    parser.add_argument('-o', '--out', default = basename(splitext(__file__)[0]),help='Name of output file')
+    parser.add_argument('--figs', default = './figs', help = 'Name of folder where plots are to be stored')
+    parser.add_argument('--show', action = 'store_true', help   = 'Show plot')
+    parser.add_argument('-N', '--N', type=int, default=100000, help='Number of iterations')
+    parser.add_argument('-i', '--input', default = 'galaxy-2025-08-09-11-42-25.log',help='Name of output file')
+    return parser.parse_args()
+
+
+def get_file_name(name,default_ext='png',figs='./figs',seq=None):
+    '''
+    Used to create file names
+
+    Parameters:
+        name          Basis for file name
+        default_ext   Extension if non specified
+        figs          Directory for storing figures
+        seq           Used if there are multiple files
+    '''
+    base,ext = splitext(name)
+    if len(ext) == 0:
+        ext = default_ext
+    if seq != None:
+        base = f'{base}{seq}'
+    qualified_name = f'{base}.{ext}'
+    return join(figs,qualified_name) if ext == 'png' else qualified_name
+
+if __name__=='__main__':
+    rc('font',**{'family':'serif','serif':['Palatino']})
+    rc('text', usetex=True)
+    start  = time()
+    args = parse_arguments()
+    rng = np.random.default_rng(args.seed)
+    pattern = compile(r'.*\((.*),(.*),(.*)\)')
+    path = join('../logs/',args.input)
+    n = 0
+    for line in open(path):
+        matched = pattern.match(line.strip())
+        if matched:
+            n += 1
+    momenta = np.zeros((n,3))
+    i = 0
+    for line in open(path):
+        matched = pattern.match(line.strip())
+        if matched:
+            for j in range(3):
+                momenta[i,j] = float(matched.group(j+1))
+            i += 1
+    momentum = momenta.sum(axis=0)
+
+    fig = figure(figsize=(12,12))
+    ax1 = fig.add_subplot(111,projection='3d')
+    ax1.scatter(momenta[:,0],momenta[:,1],momenta[:,2],s=1)
+    ax1.set_title(f'({momentum[0]},{momentum[1]},{momentum[2]})')
+    fig.suptitle(args.input)
+    fig.savefig(get_file_name(args.out,figs=args.figs))
+    elapsed = time() - start
+    minutes = int(elapsed/60)
+    seconds = elapsed - 60*minutes
+    print (f'Elapsed Time {minutes} m {seconds:.2f} s')
+
+    if args.show:
+        show()
